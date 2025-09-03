e986553f9b96da01fcfeea6fca6fe83560f82ca3
jferperez27 <juan_j_fernandez@brown.edu>
2025-09-03 17:46:43 -0400

    feat(model): add trained model v2 and v3 with 83% and 86% test accuracies, consecutively

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 SEED = 42
 BATCH_SIZE = 32
-EPOCHS = 1
+EPOCHS = 10
 IMG_SIZE = (512, 384)
 OUTPUT_DIR = "models"
 CLASS_NAMES = ["cardboard", "glass", "metal", "paper", "plastic", "trash"]
@@ -12,4 +12,4 @@ MOVE = False
 DATA_DIR = Path("data/raw/dataset-resized")
 TRAIN_DIR = Path("data/processed/train")
 TEST_DIR = Path("data/processed/test")
-MODEL_NAME = "trash_classifier_model_v1.keras"
+MODEL_NAME = "trash_classifier_model_v3.keras"
--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -7,12 +7,12 @@ all_test_images = list(TEST_DIR.rglob("*.jpg"))
 all_test_names = [img.parent.name for img in all_test_images]
 
 def test_model_accuracy():
-    model_path = Path("models/trash_classifier_model_v1.keras")
+    model_path = Path("models/trash_classifier_model_v3.keras")
 
     all_test_images = list(TEST_DIR.rglob("*.jpg"))
     all_test_names = [img.parent.name for img in all_test_images]
 
     model = keras.models.load_model(model_path)
     accuracy = model_accuracy(model, all_test_images, all_test_names, CLASS_NAMES)
-    assert 0.7 <= accuracy <= 1.0  # Accuracy should be between
+    assert 0.866 <= accuracy <= 1.0  # Accuracy should be between
 
