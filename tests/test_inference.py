3d098204def787caa9e50d1646dd234e309fe615
jferperez27 <juan_j_fernandez@brown.edu>
2025-09-04 16:11:53 -0400

    fix: update model file location in testing file local variables

--- tests/test_inference.py
+++ tests/test_inference.py
@@ -2,11 +2,11 @@ import tensorflow as tf
 from tensorflow import keras
 
 def test_model_loads():
-    model = keras.models.load_model("models/trash_classifier_model_v1.keras")
+    model = keras.models.load_model("models/trash_classifier_model_v3.keras")
     assert isinstance(model, tf.keras.Model)
 
 def test_model_predicts():
-    model = keras.models.load_model("models/trash_classifier_model_v1.keras")
+    model = keras.models.load_model("models/trash_classifier_model_v3.keras")
     dummy_input = tf.random.uniform((1, 512, 384, 3))
     predictions = model.predict(dummy_input)
     assert predictions.shape == (1, 6)  # Assuming 6 classes as per CLASS_NAMES
--- tests/test_model.py
+++ tests/test_model.py
@@ -7,7 +7,7 @@ all_test_images = list(TEST_DIR.rglob("*.jpg"))
 all_test_names = [img.parent.name for img in all_test_images]
 
 def test_model_predicts():
-    model = keras.models.load_model("models/trash_classifier_model_v1.keras")
+    model = keras.models.load_model("models/trash_classifier_model_v3.keras")
     dummy_input = tf.random.uniform((1, 512, 384, 3))
     predictions = model.predict(dummy_input)
     assert predictions.shape == (1, len(CLASS_NAMES))
